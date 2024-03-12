c135942c13f8bfc78c61c64eb89147343dcdaa10
Fastattack <120670645+fastattackv@users.noreply.github.com>
2024-03-12 21:19:15 +0100

    Updated SmoothFrame
    - Added smooth_place arguments for the final place
    - Corrected smooth_grid animation with padding

--- Source Code/SmoothFrame.py
+++ Source Code/SmoothFrame.py
@@ -15,16 +15,20 @@ def _get_coordinates_of_frame(frame):
     frame.destroy()
 
 
-def get_coordinates_from_grid(master, column: int, row: int) -> tuple[int, int]:
+def get_coordinates_from_grid(master, column: int, row: int, padx=0, pady=0, ipadx=0, ipady=0) -> tuple[int, int]:
     """Returns the coordinates of the given grid cell
 
     :param master: master to search the coordinates in
     :param column: column of the cell to obtain the coordinates from
     :param row: row of the cell to obtain the coordinates from
+    :param padx: padding in x direction
+    :param pady: padding in y direction
+    :param ipadx: internal padding in x direction
+    :param ipady: internal padding in y direction
     :return: tuple containing the x and y coordinates of the given grid cell
     """
     temp_frame = ctk.CTkFrame(master, fg_color="transparent", height=1, width=1)
-    temp_frame.grid(row=row, column=column, sticky="nw")
+    temp_frame.grid(row=row, column=column, sticky="nw", padx=padx, pady=pady, ipadx=ipadx, ipady=ipady)
     temp_frame.after(20, lambda: _get_coordinates_of_frame(temp_frame))
     temp_frame.wait_window()
     return _x, _y
@@ -68,7 +72,14 @@ class SmoothFrame(ctk.CTkFrame):
 
         # final place
         self.place(
-            # fill parameters
+            x=self.final_parameters[0],
+            y=self.final_parameters[1],
+            relx=self.final_parameters[2],
+            rely=self.final_parameters[3],
+            anchor=self.final_parameters[4],
+            relwidth=self.final_parameters[5],
+            relheight=self.final_parameters[6],
+            bordermode=self.final_parameters[7]
         )
 
         # resetting variables
@@ -78,13 +89,19 @@ class SmoothFrame(ctk.CTkFrame):
         self.coordinates_modifiers = (0, 0)
         self.final_parameters.clear()
 
-    def smooth_place(self, time_to_move: float, x: int, y: int, frequency=120):
+    def smooth_place(self, time_to_move: float, x: int, y: int, relx=0, rely=0, anchor="nw", relwidth: int = "", relheight: int = "", bordermode="inside", frequency=120):
         """Moves the frame at the given coordinates in the given time
 
         :param time_to_move: duration of the animation in seconds (do not put less than 0.05 or the animation might go crazy)
+        :param frequency: frequency of the places per second = "smoothness" of the animation (120Hz by default), should be set to the monitor's refresh rate if possible
         :param x: x coordinates to place the frame at
         :param y: y coordinates to place the frame at
-        :param frequency: frequency of the places per second = "smoothness" of the animation (120Hz by default), should be set to the monitor's refresh rate if possible
+        :param relx: locate anchor of this widget between 0.0 and 1.0 relative to width of master (1.0 is right edge)
+        :param rely: locate anchor of this widget between 0.0 and 1.0 relative to height of master (1.0 is bottom edge)
+        :param anchor: "nsew" (or subset) - position anchor according to given direction
+        :param relwidth: width of this widget between 0.0 and 1.0 relative to width of master (1.0 is the same width as the master)
+        :param relheight: height of this widget between 0.0 and 1.0 relative to height of master (1.0 is the same height as the master)
+        :param bordermode: "inside" or "outside" - whether to take border width of master widget into account
         """
         place_infos = self.place_info()
         if place_infos:  # has been placed
@@ -92,13 +109,18 @@ class SmoothFrame(ctk.CTkFrame):
             self.remaining_cycles = time_to_move * frequency
             self.current_coordinates = (int(place_infos["x"]), int(place_infos["y"]))
             self.coordinates_modifiers = ((x - self.current_coordinates[0]) / self.remaining_cycles, (y - self.current_coordinates[1]) / self.remaining_cycles)
-            self.final_parameters = [
-                # fill parameters
-            ]
+            self.final_parameters = [x, y, relx, rely, anchor, relwidth, relheight, bordermode]
             threading.Thread(target=self._smooth_place).start()
         else:  # has not already been placed
             self.place(
-                # fill params
+                x=x,
+                y=y,
+                relx=relx,
+                rely=rely,
+                anchor=anchor,
+                relwidth=relwidth,
+                relheight=relheight,
+                bordermode=bordermode
             )
 
     def _smooth_grid(self):
@@ -150,7 +172,7 @@ class SmoothFrame(ctk.CTkFrame):
             start_x = self.winfo_x()
             start_y = self.winfo_y()
             self.place(x=start_x, y=start_y)  # removes the widget from the grid so the end coordinates are correct
-            x, y = get_coordinates_from_grid(self.master, column, row)
+            x, y = get_coordinates_from_grid(self.master, column, row, padx, pady, ipadx, ipady)
 
             self.time_between_each_cycle = 1 / frequency - 0.00034  # the 0.00034 is the approximate time each placement takes, it is substracted to the time between each cycle so the global time is closer to the time_to_move
             self.remaining_cycles = time_to_move * frequency
